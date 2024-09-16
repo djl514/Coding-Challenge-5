70131f000cbf0d949d91126759d8fe1396bcb905
David Layne <davidlayne@usf.edu>
2024-09-16 19:41:46 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,11 @@
+//1. Create an Inventory Array of Product Objects
+const inventory = [
+    { name: 'Scotch', price: 6, quantity: 11 },
+    { name: 'Ale', price: 4, quantity: 5 },
+    { name: 'Bourbon', price: 5, quantity: 8 },
+    { name: 'Martini', price: 3, quantity: 3 },
+    { name: 'Margarita', price: 2, quantity: 6 }  
+];
+
+
+
