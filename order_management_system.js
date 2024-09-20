cd43a83d64ccd2aabca2ec45cfaabdd72f0d6387
David Layne <davidlayne@usf.edu>
2024-09-19 21:45:28 -0400

    Functions placeOrder and calculateOrderTotal Overhaul and fix

--- order_management_system.js
+++ order_management_system.js
@@ -8,61 +8,42 @@ const inventory = [
 ];
 
 //2. Create an Orders Array of Order Objects
-let orders = [];
+let orders = [
+
+];
 
 //3. Create a function to place an order
-function placeOrder(customerName, orderedItems, purchaseAmount){
-    let item = inventory.find(({ name }) => name === orderedItems);
-
-    if (item && item.quantity >= purchaseAmount){
-        let newOrder = { 
-            name: customerName, 
-            inventoryItem: orderedItems, 
-            purchased: purchaseAmount, 
-            status: 'Pending' };    
-        orders.push(newOrder);
-        item.quantity -= purchaseAmount;
-        
-        return newOrder;
-
-    } else if (!item){
-        return `${orderedItems} is not available`;
-
-    } else{
-        return `Insufficient Stock of ${orderedItems}`;
+function placeOrder(customerName, orderedItems){
+    for (item of orderedItems){
+        const product = inventory.find((product) => product.name == item.name);
+        if (!product){
+            return `Error: ${item.name} is not in the inventory`;
+        }
+        if (product.quantity < item.quantity){
+            return `Error: Low stocks of ${item.name}`;
+        }
     }
+    const product = inventory.find((product) => product.name == item.name);
+    product.quantity -= item.quantity;
+        orders.push({
+            custName: customerName,
+            itemsPurchased: orderedItems,
+            status: 'Pending'
+    });
 }
 
-placeOrder('David', 'Margarita', 4);
-placeOrder('Martha', 'Martini', 1);
-placeOrder('James', 'Whiskey', 2);
-placeOrder('Ian', 'Bourbon', 100);
+placeOrder('David', [{name: 'Scotch', quantity: 3}]);
+placeOrder('Kailey', [{name: 'Margarita', quantity: 2}]);
 console.log(orders);
 
-//4. Create a Function to calculate total for an order
-function calculateOrderTotal(orders){
-    let invPrice = orders.find((prod) => prod.name)
-    const totalValue = inventory.reduce((sum, product) => 
-        sum + (invPrice.purchased*product.price), 0);
-    return totalValue;
 
+//4. create a function to calculate total for an order
+function calculateOrderTotal(order){
+    let total =  order.itemsPurchased.reduce((sum, item) => {
+        const product = inventory.find(product=>product.name==item.name);
+        return sum + (product.price * item.quantity)},0);
+    return `Your total is $${total}`;
 }
 
 console.log(calculateOrderTotal(orders[1]));
 
-//5. Create a Function to mark an order as completed
-function completeOrder(customerName){
-    let customer = orders.find(({ name }) => name == customerName);
-    if (customer.status = 'Pending'){
-        customer.status = 'Completed';
-        return customer;
-    } else {
-        return `Error: ${customerName} not in system`;
-    }
-
-}
-
-console.log(completeOrder('David'));
-console.log(orders);
-
-//6. Create a Function to Check Pending Orders
